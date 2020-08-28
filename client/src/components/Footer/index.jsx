40cbba6edc78ca3826e09e816a107286237cd4b7
Melissa Flores <mel.floresp@gmail.com>
2020-08-28 13:00:24 -0500

    adding footer component. Still a WIP

---
+++ client/src/components/Footer/index.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./styles.css"
+// import {Link} from "react-router-dom";
+// import Button from "../Button"
+
+function Footer(){
+    return (
+        <div className="footer">
+        
+        </div>
+    )
+}
+
+export default Footer;
