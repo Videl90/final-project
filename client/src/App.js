9a662d3a64af87f7175505eaddb30eca6b84071e
Melissa Flores <mel.floresp@gmail.com>
2020-08-27 20:13:26 -0500

    added components to app.js file

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,14 @@
 import React from 'react';
-
+// import './App.css';
+import Navbar from "./components/Navbar";
+import Form from "./components/Form";
+import Wrapper from "./components/Wrapper";
 function App() {
   return (
-    <h1>Project 3</h1>
-  );
+    <Wrapper>
+      <Navbar></Navbar>
+      <Form></Form>
+    </Wrapper>
+  )
 }
-
 export default App;
