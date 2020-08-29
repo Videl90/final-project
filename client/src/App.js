08d7e40540e9c5dbc486a09cea14bed98f88b9f9
Melissa Flores <mel.floresp@gmail.com>
2020-08-29 13:27:02 -0500

    created newetrip page and component

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Route, Switch} from "react-router-dom";
 import Wrapper from "./components/Wrapper";
 import SignUp from "./pages/SignUp";
 import Login from "./pages/Login";
-// import NewTrip from "./pages/NewTrip";
+import NewTrip from "./pages/NewTrip";
 // import FlightResults from "./pages/FlightResults";
 // import Agenda from "./pages/Agenda";
 
@@ -16,8 +16,8 @@ function App() {
         <Switch>
         <Route exact path="/" component={SignUp} />
         <Route exact path="/login" component={Login} />
-        {/* <Route exact path="/newtrip" component={NewTrip} />
-        <Route exact path="/flight-results" component={FlightResults} />
+        <Route exact path="/newtrip" component={NewTrip} />
+        {/* <Route exact path="/flight-results" component={FlightResults} />
         <Route exact path="/agenda" component={Agenda} /> */}
         </Switch>
       </div>
